65a20e93773aea157cdcc82a1352f1b05ccb07ee
Nicholas Fordyce <FordyceNick@gmail.com>
2017-05-22 22:19:32 -0400

    Added prop array with link info

--- src/Links.js
+++ src/Links.js
@@ -34,4 +34,27 @@ class Links extends Component {
     }
 }
 
+class Link extends Component {
+
+}
+
+<Links linkinfo={[
+    {
+        src: "https://www.enclavepublishing.com/wp-content/uploads/2015/11/tumblr_inline_mtvwr6T4qH1r8eb4v.gif",
+        text: "Single Orcs in Indianapolis"
+    },
+    {
+        src: "https://images.fineartamerica.com/images-medium-large-5/rocky-mountain-landscape-meadow-in-spring-western-panorama-with-wildflowers-square-format-walt-curlee.jpg",
+        text: "You won't believe what's under this mountain"
+    },
+    {
+        src: "http://orig01.deviantart.net/a278/f/2010/357/9/7/seamless___gold_coins_by_bartalon-d35iydr.jpg",
+        text: "Mine 20% more gold with One Weird Trick"
+    },
+    {
+        src: "http://cdn23.us1.fansshare.com/photos/thehobbit/the-hobbit-square-characters-833282558.jpg",
+        text: "Surprise for Indiana Hobbits born before 1999"
+    }
+]}/>
+
 export default Links
